e4dac28b02b9b6d9aca6350d38e56936570f89b0
Thomas <1688389+Rakambda@users.noreply.github.com>
2023-06-09 20:30:58 +0200

    Update display-timer-keybinds.lua

--- display-timer-keybinds.lua
+++ display-timer-keybinds.lua
@@ -1,15 +1,28 @@
-function displayTimer()
+function setOsdConfig()
     mp.command("set osd-scale 0.5")
     mp.command("set osd-align-y left")
     mp.command("set osd-align-y bottom")
-    mp.command("set osd-level 2")
-    mp.osd_message("Timer shown")
 end
 
-function hideTimer()
-    mp.command("set osd-level 0")
-    mp.osd_message("Timer hidden")
+function setOsdLevel(level)
+    setOsdConfig()
+    current_level = mp.get_property_number("osd-level")
+    diff = current_level - level
+    if -0.1 < diff and diff < 0.1 then
+        level = 0
+    end
+    mp.commandv("set", "osd-level", level)
+end
+
+function displayTimer()
+    setOsdConfig()
+    setOsdLevel(2)
+end
+
+function displayAllTimer()
+    setOsdConfig()
+    setOsdLevel(3)
 end
 
 mp.add_forced_key_binding("t", "displayTimer", displayTimer)
-mp.add_forced_key_binding("y", "hideTimer", hideTimer)
+mp.add_forced_key_binding("y", "displayAllTimer", displayAllTimer)
